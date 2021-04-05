e3852db344319ce400fae2f7d688dbb56bf69093
Ben Taljaard <benfrancotaljaard@gmail.com>
2021-04-05 16:11:34 +0200

    first commit

---
+++ src/main/java/org/acme/Hello.java
@@ -0,0 +1,17 @@
+package org.acme;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class Hello {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        String osType= System.getProperty("os.arch");
+        return "Hello QIoT hackfest, I am running on " + osType;
+    }
+}
